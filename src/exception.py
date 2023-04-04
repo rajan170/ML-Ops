af458368d75a2d63b9768988a0c769f85f188e5d
Rajan <rajan.bangar83@gmail.com>
2023-04-05 00:01:54 +0530

    Added logging and exception capabilities

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import logging
+import sys
+# import logger
+
+
+def error_message_detail(error, err_detail: sys):
+    _, _, exc_tb = err_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occurred in file: [{0}, line:[{1}]. Error:[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error))
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, err_detail: sys):
+        super().__init__(error_message)
+
+        self.error_message = error_message_detail(error_message, err_detail = err_detail)
+
+    def __str__(self):
+
+        return self.error_message
+
+
