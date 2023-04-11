0ab77eaeba261a9c3fd852a1b7361a9ea30cba95
Rajan <rajan.bangar83@gmail.com>
2023-04-11 23:55:12 +0530

    Model training and data ingestion

--- src/Components/model_trainer.py
+++ src/Components/model_trainer.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import (
     RandomForestRegressor,
 )
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -27,7 +28,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Splitting training and test input")
             X_train, y_train, X_test, y_test = (
@@ -47,7 +48,7 @@ class ModelTrainer:
                 "AdaBoost Classifier" : AdaBoostRegressor()
             }
 
-            model_report: dict = evaluate_model(X_train=X_train, x_test=X_test, y_train=y_train, y_test=y_test, models=models)
+            model_report: dict = evaluate_model(X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, models=models)
 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
@@ -64,6 +65,10 @@ class ModelTrainer:
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
+
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
 
         except Exception as e:
             raise CustomException(sys, e)
--- src/utils.py
+++ src/utils.py
@@ -31,7 +31,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_train, y_test_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
