45a0c898fb10793d3705a7e1906c91e51e8ca4f2
Rajan <rajan.bangar83@gmail.com>
2023-04-11 23:34:20 +0530

    Model trainer pipeline component complete

---
+++ src/Components/model_trainer.py
@@ -0,0 +1,69 @@
+import os
+import sys
+from dataclasses import dataclass
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import evaluate_model
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+        try:
+            logging.info("Splitting training and test input")
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                "Random Forest" : RandomForestRegressor(),
+                "Decision Tree" : DecisionTreeRegressor(),
+                "Gradient Boosting" : GradientBoostingRegressor(),
+                "Linear Regression" : LinearRegression(),
+                "K-Neighbours Classifier" : KNeighborsRegressor(),
+                "XGB Classifier" : XGBRegressor(),
+                "CatBoosting Classifier" : CatBoostRegressor(),
+                "AdaBoost Classifier" : AdaBoostRegressor()
+            }
+
+            model_report: dict = evaluate_model(X_train=X_train, x_test=X_test, y_train=y_train, y_test=y_test, models=models)
+
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("Model accuracy below 60%")
+            logging.info(f"Best performing model on both test and train data.")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+        except Exception as e:
+            raise CustomException(sys, e)
